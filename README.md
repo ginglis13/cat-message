18d5ef3a23e502db93bc6f00c13c2f1456c22544
ginglis <ginglis05@gmail.com>
2019-05-18 01:01:53 -0400

    transferring from old repo

---
+++ README.md
@@ -0,0 +1,27 @@
+# cat-message
+gets images/gifs/videos of cats from reddit, sends to mom in applescript
+
+## Subreddits
+
+- [cats](https://www.reddit.com/r/cats)
+- [catsstandingup](https://www.reddit.com/r/catsstandingup)
+- [catslaps](https://www.reddit.com/r/catslaps)
+- [catsareassholes](https://www.reddit.com/r/catsareassholes)
+- [chonkers](https://www.reddit.com/r/chonkers)
+- [bigcatgifs](https://www.reddit.com/r/bigcatgifs)
+- [babybigcatgifs](https://www.reddit.com/r/babybigcatgifs)
+
+## Checklist
+
+- [X] Find and save jpgs
+- [X] Handle gyfcat posts
+- [X] Handle imgur posts
+- [ ] Handle YouTube posts
+- [X] Handle other image formats (png, etc)
+- [X] Write the applescript
+- [ ] Include the post title in iMessage
+- [ ] Comment Code, clean up, document
+
+## Shortcomings
+
+Unfortunately, requesting videos from reddit (sources that include `v.red.it`) resulted in reddit detecting that a script was making too many requests to the site. Since posts of that format from these subreddits seems to be a rarity, I will not be attempting a workaround.
---
+++ get_cats.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+'''
+get_cats.py
+
+a script to get cat images, gifs, and vids from reddit
+doing this so i can send them to my mom using an applescript
+
+May 2019
+ginglis
+'''
+
+import requests
+import random
+import pprint
+import os
+from io import open as iopen
+from urllib.parse import urlsplit
+
+'''Globals/Metadata'''
+
+SUBREDDITS = [
+        'cats',
+        'catsstandingup',
+        'catslaps',
+        'catsareassholes',
+        'chonkers',
+        'bigcatgifs',
+        'babybigcatgifs'
+        ]
+
+REDDIT_URL = 'https://reddit.com/r/{}/.json'
+GFYCAT = 'https://gfycat.com/cajax/get/{}'
+
+headers = {'user-agent': 'reddit-{}'.format(os.environ.get('USER', 'cse-20289-sp19'))}
+
+'''Functions'''
+
+def dl_content(url, source):
+    if source == 'gfycat.com':
+        gfy_url = url.split('/')[-1]
+
+        r = requests.get(GFYCAT.format(gfy_url)).json()
+        gfy_mp4url = r['gfyItem']['mp4Url']
+
+        return gfy_mp4url
+
+    elif source == 'i.imgur.com':
+        img_url = url.replace('.gifv', '.mp4')
+        return img_url
+
+    else:
+        return url
+
+def handle_url(url=REDDIT_URL):
+
+    url      = url.format(random.choice(SUBREDDITS))
+    response = requests.get(url, headers=headers).json()
+    data     = response['data']['children']
+    images   = []
+
+    for post in data:
+
+        d = {}
+        if post['data']['stickied'] == True:
+            continue
+
+        url    = post['data']['url']
+        title  = post['data']['title']
+        source = post['data']['domain']
+
+        d[url]=source
+
+        images.append(d)
+
+    img        = random.choice(images)
+    img_url    = next(iter(img.keys()))
+    img_source = next(iter(img.values()))
+
+    print('Using url: {}'.format(img_url))
+
+    return dl_content(img_url, img_source)
+
+def write_file(img_url):
+    print('img url: {}'.format(img_url))
+    if img_url:
+        if 'mp4' in img_url:
+            ext = '.mp4'
+        elif 'png' in img_url:
+            ext = '.png'
+        elif 'gif' in img_url:
+            ext = '.gif'
+        elif 'jpeg' in img_url:
+            ext = '.jpeg'
+        elif 'v.redd.it' in img_url:
+            print('The source of this file is v.reddit. Unfortunately, reddit recognizes requests to this source as being from a script and blocks them. Apologies.')
+        else:
+            ext = '.jpg'
+
+        fname = 'cat' + ext
+        r = requests.get(img_url, stream=True)
+        with iopen(fname, 'wb') as file:
+            file.write(r.content)
+    else:
+        print('Something went wrong - no url')
+
+'''Main Execution'''
+
+# remove former cat file, necessary for applescript
+os.popen('rm ~/projects/cat-message/cat*')
+
+img = handle_url(REDDIT_URL)
+write_file(img)
