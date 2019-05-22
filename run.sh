1177880f401b845cbec15621c34005fd069ce8da
Gavin Inglis <43075615+ginglis13@users.noreply.github.com>
2019-05-22 13:23:55 -0400

    remove sleep delay

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # capture output of script to pass info to applescript
 # this is done to send the title of the post
-var=$(./get_cats.py); sleep 5; osascript msg_cat.scpt "$var"
+var=$(./get_cats.py); osascript msg_cat.scpt "$var"
